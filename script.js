9b070fd33fef6d87aa8e578364d67fee4449b9a2
mrndolo <callistomutunga@gmail.com>
2024-05-21 14:44:39 +0300

    coded the questions logic

---
+++ script.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+      id: 1,
+      question: "Which of these fish is actually a fish?",
+      answers: [
+        { answer: "swordfish", isCorrect: true },
+        { answer: "jellyfish", isCorrect: false },
+        { answer: "starfish", isCorrect: false },
+        { answer: "crayfish", isCorrect: false },
+      ],
+    },
+    {
+      id: 2,
+      question: "A flutter is a group of:",
+      answers: [
+        { answer: "bees", isCorrect: false },
+        { answer: "penguins", isCorrect: false },
+        { answer: "butterflies", isCorrect: true },
+        { answer: "camels", isCorrect: false },
+      ],
+    },
+    {
+      id: 1,
+      question: "A group of which animals is referred to as a wake?",
+      answers: [
+        { answer: "bats", isCorrect: false },
+        { answer: "vultures", isCorrect: true },
+        { answer: "ants", isCorrect: false },
+      ],
+    },
+  ];
+
+  const gameScreen = document.querySelector(".game")
+  const resultScreen = document.querySelector(".result")
+  const question = document.querySelector(".question")
+  const answersContainer = document.querySelector(".answers")
+  const submit = document.querySelector(".submit")
+  const play = document.querySelector(".play")
+
+  let qIndex = 0;
+  let correctCount = 0;
+  let wrongCount = 0;
+  let total = 0;
+  let selectedAnswer;
+
+// Sow results page
+const showResult = ()=>{
+    resultScreen.style.display = "block"
+    gameScreen.style.display = "none"
+}
+
+// Function that shows the question
+const showQuestion = (qNumber) =>{
+    if(qIndex === data.length) return showResult();
+    selectedAnswer = null;
+    question.textContent = data[qNumber].question;
+    answersContainer.innerHTML = data[qNumber].answers.map((item, index)=>
+        `
+        <div class="answer">
+            <input name="answer" type="radio" id=${index} value=${item.isCorrect}>
+            <label for=${index}>${item.answer}</label>
+        </div>
+        `
+    ).join("");
+
+    selectAnswer()
+};
+
+const selectAnswer = ()=>{
+    answersContainer.querySelectorAll("input").forEach(el=>{
+        el.addEventListener("click", (e)=>{
+            // console.log(e.target.value)
+            selectedAnswer = e.target.value;
+        })
+    })
+}
+
+const submitAnswer = ()=>{
+    submit.addEventListener("click", ()=>{
+        if(selectedAnswer !== null){
+            selectedAnswer === "true" ? correctCount++ : wrongCount++;
+            qIndex++;
+            showQuestion(qIndex);
+        }else alert("Select an answer!")
+        
+    })
+}
+
+showQuestion(qIndex);
+submitAnswer();
